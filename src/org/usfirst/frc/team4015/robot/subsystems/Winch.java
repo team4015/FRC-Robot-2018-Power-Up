5e74c449a14acbe496eddfb429ef135005795586
FRC Team 4015 "Jag <jags4015@gmail.com>
2018-02-12 23:35:06 -0500

    Update Winch.java

--- src/org/usfirst/frc/team4015/robot/subsystems/Winch.java
+++ src/org/usfirst/frc/team4015/robot/subsystems/Winch.java
@@ -2,7 +2,7 @@ package org.usfirst.frc.team4015.robot.subsystems;
 
 import org.usfirst.frc.team4015.robot.RobotMap;
 
-import edu.wpi.first.wpilibj.Talon;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /* ===================================================
@@ -12,18 +12,17 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Winch extends Subsystem
 {
-	// Talons here - make one object to control both sides
-	Talon talon;
+	
 	public Winch()
 	{
-		talon = new Talon(RobotMap.winchMotor);
+		
 	}
 	
 	// SPIN WINCH TO CLIMB UP //
 	
 	public void spin()
 	{
-		talon.set(1.0);
+		
 	}
 	
 	// STOP WINCH //
